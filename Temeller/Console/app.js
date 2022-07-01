c9fb37eb8d9c9462fddae62de9451b979327d1f3
Kerem Metin <k.metin01@gmail.com>
2022-07-01 23:51:18 +0300

    files

---
+++ Temeller/Console/app.js
@@ -0,0 +1,18 @@
+// console.log("Merhaba Javascript");
+// console.log(123123);
+// console.log(true);
+
+
+// var a = 10;
+
+// console.log(a);
+
+// console.log(["Ahmet","Mehmet"]);
+
+// console.log(typeof a);
+
+
+
+console.warn("Bu bir uyarıdır.")
+console.error("Hataaaaaaa..............");
+console.clear();
